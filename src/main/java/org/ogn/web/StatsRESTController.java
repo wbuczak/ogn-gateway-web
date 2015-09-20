64117d9460b5e4095ba93a8c4ff4b2cbb78e0b42
wbuczak <wojciech.buczak@gmail.com>
2015-09-20 21:37:48 +0200

    support for max_alt stats

--- src/main/java/org/ogn/web/StatsRESTController.java
+++ src/main/java/org/ogn/web/StatsRESTController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.ogn.gateway.plugin.stats.TimeDateUtils;
 import org.ogn.gateway.plugin.stats.dao.StatsDAO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,43 +23,54 @@ public class StatsRESTController {
 	}
 
 	@RequestMapping(value = "/activerec/{days}", method = RequestMethod.GET)
-	public List<Map<String, Object>> jsonActiveReceivers(Model model, @PathVariable("days") int days) {
+	public List<Map<String, Object>> jsonActiveReceivers(@PathVariable("days") int days) {
 		return dao.getActiveReceiversCount(days);
 	}
 
 	@RequestMapping(value = "/activerec-all", method = RequestMethod.GET)
-	public  List<Map<String, Object>> jsonActiveReceivers(Model model) {
-		return jsonActiveReceivers(model,0);
+	public List<Map<String, Object>> jsonActiveReceivers() {
+		return jsonActiveReceivers(0);
 	}
 
 	@RequestMapping(value = "/toprange/{count}", method = RequestMethod.GET)
-	public List<Map<String, Object>> jsonTopRangeReceivers(Model model, @PathVariable("count") int count) {
+	public List<Map<String, Object>> jsonTopRangeReceivers(@PathVariable("count") int count) {
 		return dao.getTopRangeRecords(count);
 	}
 
 	@RequestMapping(value = "/toprange-all/{date}", method = RequestMethod.GET)
-	public List<Map<String, Object>> jsonTopRangeReceivers(Model model, @PathVariable("date") String date) {
-		return jsonTopRangeReceivers(model, date, 0);
+	public List<Map<String, Object>> jsonTopRangeReceivers(@PathVariable("date") String date) {
+		return jsonTopRangeReceivers(date, 0);
 	}
 
 	@RequestMapping(value = "/toprange/{date}/{count}", method = RequestMethod.GET)
-	public List<Map<String, Object>> jsonTopRangeReceivers(Model model, @PathVariable("date") String date,
+	public List<Map<String, Object>> jsonTopRangeReceivers(@PathVariable("date") String date,
 			@PathVariable("count") int count) {
-		long d = TimeDateUtils.fromString(date);		
+		long d = TimeDateUtils.fromString(date);
 		return dao.getTopRangeRecords(d, count);
 	}
-	
-	
+
 	@RequestMapping(value = "/topcount/{date}/{count}", method = RequestMethod.GET)
-	public List<Map<String, Object>> jsonTopCountReceivers(Model model, @PathVariable("date") String date,
+	public List<Map<String, Object>> jsonTopCountReceivers(@PathVariable("date") String date,
 			@PathVariable("count") int count) {
-		long d = TimeDateUtils.fromString(date);		
+		long d = TimeDateUtils.fromString(date);
 		return dao.getTopCountRecords(d, count);
 	}
-	
+
 	@RequestMapping(value = "/topcount-all/{date}", method = RequestMethod.GET)
-	public List<Map<String, Object>> jsonTopCountReceivers(Model model, @PathVariable("date") String date) {
-		return jsonTopCountReceivers(model,date,0);
+	public List<Map<String, Object>> jsonTopCountReceivers(@PathVariable("date") String date) {
+		return jsonTopCountReceivers(date, 0);
+	}
+
+	@RequestMapping(value = "/topalt-all/{date}", method = RequestMethod.GET)
+	public List<Map<String, Object>> jsonTopAltReceivers(@PathVariable("date") String date) {
+		return jsonTopAltReceivers(date, 0);
+	}
+
+	@RequestMapping(value = "/topalt-all/{date}/{limit}", method = RequestMethod.GET)
+	private List<Map<String, Object>> jsonTopAltReceivers(@PathVariable("date") String date,
+			@PathVariable("count") int limit) {
+		long d = TimeDateUtils.fromString(date);
+		return dao.getTopAltRecords(d, limit);
 	}
 
 }
