1956d994748b8b656024256d305f42c4a9bfab2f
buczakwk <buczakwk@skysoft-atm.com>
2018-05-14 17:40:20 +0200

    added function to print out file size in human readable format

--- src/main/resources/static/func.js
+++ src/main/resources/static/func.js
@@ -16,3 +16,8 @@ var params = function() {
        var prmstr = window.location.search.substr(1);
        return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
      }();
+	 
+function humanFileSize(size) {
+    var i = Math.floor( Math.log(size) / Math.log(1024) );
+    return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+};
